cb810530e13b011fd775e6a9ebb75c7793b9928e
park jong ik <pji219@naver.com>
2021-05-01 23:52:33 +0900

    오타 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import Login from './components/login/login';
-import style from './app.module.css';
+import styles from './app.module.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Maker from './components/maker/maker';
 
 function App({ authService }) {
   return (
-    <div className={style.app}>
+    <div className={styles.app}>
       <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Login authService={authService} />
-        </Route>
-        <Route path="/maker">
-          <Maker authService={authService} />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
